2742d5be68c6df2d1770e366af65c07dfe768ff9
Hovhannes <hovhannes@bizspice.com>
2020-04-01 01:47:20 +0400

    changes

--- covid-19.py
+++ covid-19.py
@@ -26,7 +26,8 @@ def mess(message):
         print(data)
     else:
         data = covid.get_status_by_country_name(getMessage)
-    replyMessage = f"COVID-19-ի վերջին տվյալները <b>{string.capwords(message.text)}-ում</b><br>Վարակված: <b>{data['confirmed']}</b><br>Ապաքինված: <b>{data['recovered']}</b><br>Մահացած: <b>{data['deaths']}</b><br>Վարակակիր: <b>{data['active']}</b><br>Վերջին Թարմացումը: <b> {datetime.datetime.fromtimestamp(data['last_update']/1000.0)}</b>"
+        #<br>Վերջին Թարմացումը: <b> {datetime.datetime.fromtimestamp(data['last_update']/1000.0)}</b>
+    replyMessage = f"COVID-19-ի վերջին տվյալները <b>{string.capwords(message.text)}-ում</b><br>Վարակված: <b>{data['confirmed']}</b><br>Ապաքինված: <b>{data['recovered']}</b><br>Մահացած: <b>{data['deaths']}</b><br>Վարակակիր: <b>{data['active']}</b>"
     bot.send_message(message.chat.id, replyMessage, parse_mode='html')
 
 bot.polling(none_stop=True)
