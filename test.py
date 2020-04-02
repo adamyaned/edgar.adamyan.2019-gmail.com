e1d62a3726a1c591e98e02488b765f4048f4803d
Hovhannes <hovhannes@bizspice.com>
2020-04-02 23:40:46 +0400

    changes

--- test.py
+++
@@ -1,6 +0,0 @@
-from covid import Covid
-import time
-covid = Covid(source="worldometers")
-data=covid.get_status_by_country_name('armenia')
-result = round((data['new_cases']/(data['confirmed']-data['new_cases']))*100, 2)
-print(result)
