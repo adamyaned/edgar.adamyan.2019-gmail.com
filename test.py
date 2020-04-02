19474e4c53baf80708f4487ba0605913e8221c73
Hovhannes <hovhannes@bizspice.com>
2020-04-02 22:41:42 +0400

    changes

--- test.py
+++ test.py
@@ -2,4 +2,5 @@ from covid import Covid
 import time
 covid = Covid(source="worldometers")
 data=covid.get_status_by_country_name('armenia')
-print(data)
+result = round(((data['confirmed']-data['new_cases'])*100)/(data['confirmed']-data['new_cases']), 2)
+print(result)
