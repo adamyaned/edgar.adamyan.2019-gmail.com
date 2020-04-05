f71638772d8da9976357bb685994a4d454ec4329
Hovhannes <hovhannes@bizspice.com>
2020-04-05 20:28:34 +0400

    changes

--- covid-19.py
+++ covid-19.py
@@ -50,7 +50,7 @@ def mess(message):
                 data = covid.get_status_by_country_name(country)
                 replyMessage = f"COVID-19-ի վերջին տվյալները <b>{string.capwords(message.text)}</b>-ում։ Երկրում կա <b>{data['confirmed']}</b> վարակված անձ որոնցից ապաքինվել է <b>{data['recovered']}({round(data['recovered']*100/(data['deaths']+data['recovered']), 2)}%)</b> մարդ, մահացել <b>{data['deaths']}({round(data['deaths']*100/(data['deaths']+data['recovered']), 2)}%)</b>-ը և այժմ բուժում է ստանում <b>{data['active']}({round(data['active']*100/data['confirmed'], 2)}%)</b> մարդ։ Վերջին մեկ օրում գրանցվել է <b>{data['new_cases']}</b> նոր դեպք, վարակվածների թվի տոկոսային աճը՝ <b>{round((data['new_cases']/(data['confirmed']-data['new_cases']))*100, 2)}%</b>"
                 bot.send_message(message.chat.id, replyMessage, parse_mode='html')
-            data.clear()
+                data.clear()
     else:
         replyMessage = "Երկրի անունը սխալ է!"
         bot.send_message(message.chat.id, replyMessage, parse_mode='html')
